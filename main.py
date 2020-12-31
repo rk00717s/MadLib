d40bddab2a2e297580c31fa5003d69601cc52c60
rk00717s <ronikmarghade@gmail.com>
2020-12-31 23:21:20 +0530

    external linking

--- main.py
+++ main.py
@@ -36,9 +36,8 @@ keypick = str(randrange(0, len(contents)+1))
 
 formating_values = list()
 
+print(f'Number of inputs to be taken : {len(contents[keypick]["inputs"])}')
 counter=0
-
-# print(f"Number of values to be taken : {len(contents[keypick]["inputs"])}")
 for op in contents[keypick]["inputs"]:
     counter+=1
     formating_values.append(input(f"{counter}. {op}"))
